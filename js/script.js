701c8f924af0ebadc5bfe0c22c59b8d1be6be5b2
I2oman <romanhrynchack@gmail.com>
2024-11-01 20:02:12 +0000

    Log in animation v2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // menu.js
 document.addEventListener("DOMContentLoaded", function () {
   updateFontHeight();
+  addAnimation();
 
   const menuIcon = document.querySelector(".menu-icon");
   const slideMenuHolder = document.querySelector(".slide-menu-holder");
@@ -24,10 +25,26 @@ window.addEventListener("resize", () => {
 function updateFontHeight() {
   const mainTag = document.querySelector("main");
   const mainHeight = mainTag.getBoundingClientRect().height;
-  const fontHeight = (mainHeight / 3) * 0.62;
+  const fontHeight = (mainHeight / 2.5) * 0.62;
   console.log(mainHeight);
   console.log(fontHeight);
-  document.querySelectorAll(".parallax-row").forEach((element) => {
+  document.querySelectorAll(".text-content li").forEach((element) => {
     element.style.fontSize = fontHeight + "px";
+  });
+  document.querySelectorAll(".text-content").forEach((element) => {
+    element.style.gap = fontHeight / 3 + "px";
+    element.style.paddingRight = fontHeight / 3 + "px";
+  });
+}
+
+function addAnimation() {
+  document.querySelectorAll(".scroller").forEach((scroller) => {
+    const scrollerInner = scroller.querySelector(".scroller_inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
   });
 }
