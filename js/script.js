9024c8f49324072ab2a1c51b1bc35f282c948aee
I2oman <romanhrynchack@gmail.com>
2024-11-01 17:03:33 +0000

    Animation update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // menu.js
 document.addEventListener("DOMContentLoaded", function () {
+  updateFontHeight();
+
   const menuIcon = document.querySelector(".menu-icon");
   const slideMenuHolder = document.querySelector(".slide-menu-holder");
   const slideMenu = document.querySelector(".slide-menu");
@@ -14,3 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+window.addEventListener("resize", () => {
+  updateFontHeight();
+});
+
+function updateFontHeight() {
+  const mainTag = document.querySelector("main");
+  const mainHeight = mainTag.getBoundingClientRect().height;
+  const fontHeight = (mainHeight / 3) * 0.62;
+  console.log(mainHeight);
+  console.log(fontHeight);
+  document.querySelectorAll(".parallax-row").forEach((element) => {
+    element.style.fontSize = fontHeight + "px";
+  });
+}
