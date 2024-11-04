fd45ea7708d18a5067822b9cd219d43b924956b4
I2oman <romanhrynchack@gmail.com>
2024-11-04 16:18:05 +0000

    New register form, media query updated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// menu.js
 document.addEventListener("DOMContentLoaded", function () {
   updateFontHeight();
   addAnimation();
@@ -21,6 +20,11 @@ document.addEventListener("DOMContentLoaded", function () {
 window.addEventListener("resize", () => {
   updateFontHeight();
 });
+
+const resizeObserver = new ResizeObserver((entries) => {
+  updateFontHeight();
+});
+resizeObserver.observe(document.querySelector("main"));
 
 function updateFontHeight() {
   const mainTag = document.querySelector("main");
@@ -48,3 +52,45 @@ function addAnimation() {
     });
   });
 }
+
+function toggleLogInRegister(type) {
+  const logInForm = document.getElementById("form-log-in");
+  const registerForm = document.getElementById("form-register");
+
+  if (type === "logIn") {
+    logInForm.classList.add("active");
+    registerForm.classList.remove("active");
+  } else if (type === "register") {
+    registerForm.classList.add("active");
+    logInForm.classList.remove("active");
+  } else {
+    logInForm.classList.remove("active");
+    registerForm.classList.remove("active");
+  }
+}
+
+function toggleFields() {
+  const userType = document.getElementById("userType").value;
+  const customerFields = document.getElementById("customerFields");
+  const charityFields = document.getElementById("charityFields");
+
+  if (userType === "customer") {
+    customerFields.classList.add("active");
+    charityFields.classList.remove("active");
+  } else if (userType === "charity") {
+    charityFields.classList.add("active");
+    customerFields.classList.remove("active");
+  } else {
+    // Clear both if no selection
+    customerFields.classList.remove("active");
+    charityFields.classList.remove("active");
+  }
+}
+
+// function changeColor() {
+//   // Select the :root element
+//   const root = document.documentElement;
+
+//   // Change the CSS variable --periwinkle to a new color
+//   root.style.setProperty("--periwinkle", "#ff6347"); // Changing to Tomato color
+// }
