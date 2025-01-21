c1432c99c8c5a5fc4b4d8ddda15e3c3b9166dc52
BougaStefa <BougaStefa@gmail.com>
2025-01-21 18:07:03 +0000

    Matched Charity to design pattern

--- ReactPart/src/pages/Charities/Charities.tsx
+++ ReactPart/src/pages/Charities/Charities.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import styles from "./Charities.module.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import { UserProvider } from "../../context/UserProvider";
+import Charity from "../../components/Charity/Charity";
+//!TODO: Get these from the db instead
 const charities = [
   {
     name: "Charity One",
@@ -10,7 +11,7 @@ const charities = [
     charityId: 1,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultrices erat et ipsum pretium volutpat. Etiam tempus at odio id luctus. Nunc nec ornare leo, in placerat libero. Aenean risus mi, posuere sed leo a, gravida feugiat orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In id sem felis. Nam sagittis interdum lectus, eget imperdiet velit viverra sit amet. Fusce suscipit et sapien vitae consequat. Curabitur faucibus, erat non mollis mattis, mauris erat auctor dui, id gravida urna erat tincidunt nibh.",
-    logo: "https://clipground.com/images/placeholder-logo-5.png"
+    logo: "https://clipground.com/images/placeholder-logo-5.png",
   },
   {
     name: "Charity Two",
@@ -18,7 +19,7 @@ const charities = [
     charityId: 2,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultrices erat et ipsum pretium volutpat. Etiam tempus at odio id luctus. Nunc nec ornare leo, in placerat libero. Aenean risus mi, posuere sed leo a, gravida feugiat orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In id sem felis. Nam sagittis interdum lectus, eget imperdiet velit viverra sit amet. Fusce suscipit et sapien vitae consequat. Curabitur faucibus, erat non mollis mattis, mauris erat auctor dui, id gravida urna erat tincidunt nibh.",
-    logo: "https://clipground.com/images/placeholder-logo-5.png"
+    logo: "https://clipground.com/images/placeholder-logo-5.png",
   },
   {
     name: "Charity Three",
@@ -26,7 +27,7 @@ const charities = [
     location: "Country, City",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultrices erat et ipsum pretium volutpat. Etiam tempus at odio id luctus. Nunc nec ornare leo, in placerat libero. Aenean risus mi, posuere sed leo a, gravida feugiat orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In id sem felis. Nam sagittis interdum lectus, eget imperdiet velit viverra sit amet. Fusce suscipit et sapien vitae consequat. Curabitur faucibus, erat non mollis mattis, mauris erat auctor dui, id gravida urna erat tincidunt nibh.",
-    logo: "https://clipground.com/images/placeholder-logo-5.png"
+    logo: "https://clipground.com/images/placeholder-logo-5.png",
   },
   {
     name: "Charity Four",
@@ -34,7 +35,7 @@ const charities = [
     charityId: 4,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultrices erat et ipsum pretium volutpat. Etiam tempus at odio id luctus. Nunc nec ornare leo, in placerat libero. Aenean risus mi, posuere sed leo a, gravida feugiat orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In id sem felis. Nam sagittis interdum lectus, eget imperdiet velit viverra sit amet. Fusce suscipit et sapien vitae consequat. Curabitur faucibus, erat non mollis mattis, mauris erat auctor dui, id gravida urna erat tincidunt nibh.",
-    logo: "https://clipground.com/images/placeholder-logo-5.png"
+    logo: "https://clipground.com/images/placeholder-logo-5.png",
   },
   // Add more charity objects as needed
 ];
@@ -44,19 +45,17 @@ export default function Charities() {
       <Header />
       <div className={styles.charityList}>
         {charities.map((charity, index) => (
-          <div
+          <Charity
             key={charity.charityId}
-            className={`${styles.charity_entry} ${index % 2 === 0 ? styles.leftAligned : styles.rightAligned}`}
-          >
-            <img src={charity.logo} alt="logo" className={styles.logo} />
-            <div className={`${styles.charity_details} ${index % 2 === 0 ? styles.leftText : styles.rightText}`}>
-              <h1 className={styles.charity_name}>{charity.name}</h1>
-              <p className={styles.charity_location}>{charity.location}</p>
-              <p className={styles.charity_description}>{charity.description}</p>
-            </div>
-          </div>
+            name={charity.name}
+            location={charity.location}
+            description={charity.description}
+            logo={charity.logo}
+            index={index}
+          />
         ))}
       </div>
-      <Footer />   </>
+      <Footer />{" "}
+    </>
   );
 }
