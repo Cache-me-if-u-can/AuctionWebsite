dad1583a9ac04c792e2cd2b0940d2daba00e811b
BougaStefa <bougastefa@gmail.com>
2025-03-25 20:46:43 +0000

    refactor(footer): update footer layout and add FAQ link
    - Improve footer structure with links section
    - Add FAQ link to footer navigation
    - Adjust footer spacing and layout

--- ReactPart/src/components/Footer/Footer.tsx
+++ ReactPart/src/components/Footer/Footer.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div>©GAVEL.XYZ all rights reserved 2024</div>
+      <div className={styles.footerContent}>
+        <nav className={styles.footerLinks}>
+          <Link to="/faq" className={styles.footerLink}>
+            FAQ
+          </Link>
+          {/* If we decide to add more footer link like Contact Us etc they go here */}
+        </nav>
+        <div className={styles.copyright}>
+          ©GAVEL.XYZ all rights reserved 2024
+        </div>
+      </div>
     </footer>
   );
 }
