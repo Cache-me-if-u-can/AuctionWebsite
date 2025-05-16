aee8446ef27d9d31ba07b3abfba6ea3f8dc39124
BougaStefa <bougastefa@gmail.com>
2025-05-16 18:29:04 +0100

    fix(security): remove hardcoded credentials

--- PythonPart/bdConnection.py
+++ PythonPart/bdConnection.py
@@ -1,5 +1,13 @@
-import pymongo 
+import pymongo
+import os
+from dotenv import load_dotenv
 
-client = pymongo.MongoClient("mongodb+srv://gavelxyz123:1234@cluster0.o2uc8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = client.gavelXYZ
+# Load environment variables
+load_dotenv()
 
+# Get MongoDB URI from environment variables
+mongodb_uri = os.getenv("MONGODB_URI")
+db_name = os.getenv("DB_NAME")
+
+client = pymongo.MongoClient(mongodb_uri)
+db = client[db_name]
